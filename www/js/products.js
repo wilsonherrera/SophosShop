dbb481a18b85adb1b41c34af3fa10290d41f2510
Arkano22 <jfgonzalezt2@gmail.com>
2015-03-12 16:21:46 -0500

    Ajuste Lista de Productos
    Ajuste Lista de Productos

--- www/js/products.js
+++ www/js/products.js
@@ -10,7 +10,11 @@ function messageHandler(jsonResponse) {
 	var listId = 0;
 	$.each(jsonProductArray, function (key, value) {
 		var idImage = value.id_default_image;
-		$('#productListView').append('<a class="list-group-item allow-badge widget uib_w_11" data-uib="twitter%20bootstrap/list_item" data-ver="1" onclick="productDetail(' + listId + ')" ><div class="grid grid-pad urow uib_row_1 row-height-1" data-uib="layout/row" data-ver="0"><div class="col uib_col_3 col-0_4-12" data-uib="layout/col" data-ver="0"><div class="widget-container content-area vertical-col"><div class="widget uib_w_12 scale-image d-margins" data-uib="media/img" data-ver="0"><figure class="figure-align"><img src="http://190.143.91.138:9191/prestashop/img/p/' + idImage + '/' + idImage + '-cart_default.jpg"><figcaption data-position="bottom"></figcaption></figure></div><span class="uib_shim"></span></div></div><div class="col uib_col_2 col-0_8-12" data-uib="layout/col" data-ver="0"><div class="widget-container content-area vertical-col"><span class="widget uib_w_13 label label-warning" data-uib="twitter%20bootstrap/badge_and_label" data-ver="1">Producto</span><span class="widget uib_w_14 label label-default" data-uib="twitter%20bootstrap/badge_and_label" data-ver="1" id="product">' + value.name + '</span><span class="widget uib_w_15 label label-warning margintop" data-uib="twitter%20bootstrap/badge_and_label" data-ver="1">Precio</span><span class="widget uib_w_16 label label-default" data-uib="twitter%20bootstrap/badge_and_label" data-ver="1" id="price">Price: $' + parseFloat(value.price).toFixed(2) + '</span><span class="uib_shim"></span></div></div><span class="uib_shim"></span></div></div></a>');
+		var name = value.name;
+		if (value.name.length > 25) {
+			name = value.name.substring(0, 25) + "...";
+		}
+		$('#productListView').append('<a class="list-group-item allow-badge widget uib_w_11" data-uib="twitter%20bootstrap/list_item" data-ver="1" onclick="productDetail(' + listId + ')" ><div class="grid grid-pad urow uib_row_1 row-height-1" data-uib="layout/row" data-ver="0"><div class="col uib_col_3 col-0_4-12" data-uib="layout/col" data-ver="0"><div class="widget-container content-area vertical-col"><div class="widget uib_w_12 scale-image d-margins" data-uib="media/img" data-ver="0"><figure class="figure-align"><img src="http://190.143.91.138:9191/prestashop/img/p/' + idImage + '/' + idImage + '-cart_default.jpg"><figcaption data-position="bottom"></figcaption></figure></div><span class="uib_shim"></span></div></div><div class="col uib_col_2 col-0_8-12" data-uib="layout/col" data-ver="0"><div class="widget-container content-area vertical-col"><span class="widget uib_w_13 label label-warning" data-uib="twitter%20bootstrap/badge_and_label" data-ver="1">Producto</span><span class="widget uib_w_14 label label-default" data-uib="twitter%20bootstrap/badge_and_label" data-ver="1" id="product">' + name + '</span><span class="widget uib_w_15 label label-warning margintop" data-uib="twitter%20bootstrap/badge_and_label" data-ver="1">Precio</span><span class="widget uib_w_16 label label-default" data-uib="twitter%20bootstrap/badge_and_label" data-ver="1" id="price">$' + parseFloat(value.price).toFixed(2) + '</span><span class="uib_shim"></span></div></div><span class="uib_shim"></span></div></div></a>');
 		listId++;
 	});
 	$('#productListView').listview('refresh');
@@ -25,6 +29,16 @@ function messageHandlerCreateProduct(response) {
 		alert("Error - " + response);
 	}
 }
+
+function messageHandlerUpdateProduct(response) {
+	waitingDialog.hide();
+	if (response === '1') {
+		alert("Producto actualizado");
+	} else {
+		alert("Error - " + response);
+	}
+}
+
 
 function mailHandler(response) {
 	waitingDialog.hide();
@@ -58,6 +72,16 @@ function setProductDetail() {
 	document.getElementById('product_desc').innerHTML = (jsonProductArray[selectedProductId].description === null) ? jsonProductArray[selectedProductId].description_short : jsonProductArray[selectedProductId].description;
 
 	localStorage.productName = jsonProductArray[selectedProductId].name;
+	var name = jsonProductArray[selectedProductId].name;
+	if (jsonProductArray[selectedProductId].name.length > 25) {
+		name = jsonProductArray[selectedProductId].name.substring(0, 25);
+	}
+	document.getElementById('product_img').src = "http://190.143.91.138:9191/prestashop/img/p/" + imgFolder + idImage + ".jpg";
+	document.getElementById('product_name').innerHTML = name;
+	document.getElementById('product_price').innerHTML = "Price: $" + jsonProductArray[selectedProductId].price;
+	document.getElementById('product_desc').innerHTML = (jsonProductArray[selectedProductId].description === null) ? jsonProductArray[selectedProductId].description_short : jsonProductArray[selectedProductId].description;
+
+	localStorage.productName = jsonProductArray[selectedProductId].name; >>> >>> > origin / master
 	localStorage.productPrice = document.getElementById('product_price').innerHTML;
 	localStorage.imgSrc = document.getElementById('product_img').src;
 }
@@ -66,6 +90,7 @@ function setProductEdit() {
 	selectedProductId = localStorage.productID;
 	jsonProductArray = JSON.parse(localStorage.jsonArray).products;
 	localStorage.productID = jsonProductArray[selectedProductId].id;
+	localStorage.idDefaultImage = jsonProductArray[selectedProductId].id_default_image;
 	var idImage = jsonProductArray[selectedProductId].id_default_image;
 	var imgFolder = "";
 	for (var i = 0; i < idImage.length; i++) {
@@ -95,7 +120,15 @@ function createProduct() {
 	if (localStorage.edit === "false") {
 		addNewProduct(JSON.stringify(jsonProductToCreate), messageHandlerCreateProduct, localStorage.imgData);
 	} else {
-		alert("editar");
+		var jsonProductToEdit = JSON.parse('{}');
+		jsonProductToEdit.id = (localStorage.productID);
+		jsonProductToEdit.idDefaultImage = localStorage.idDefaultImage;
+		jsonProductToEdit.price = (document.getElementById('product_price').value);
+		jsonProductToEdit.quantity = (document.getElementById('product_quantity').value);
+		jsonProductToEdit.name = (document.getElementById('product_name').value);
+		jsonProductToEdit.desc = (document.getElementById('product_longdesc').value);
+		jsonProductToEdit.desc_short = (document.getElementById('product_shortdesc').value);
+		updateProduct(JSON.stringify(jsonProductToEdit), messageHandlerUpdateProduct, localStorage.imgData);
 	}
 }
 
